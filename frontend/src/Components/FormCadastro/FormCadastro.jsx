223e77e3dc4cb495f9b11dd17776ffc911664c23
Frederico Thomaz <fredericoufsj@yahoo.com.br>
2022-03-19 12:25:20 -0300

    integração pagina cadastro

--- frontend/src/Components/FormCadastro/FormCadastro.jsx
+++ frontend/src/Components/FormCadastro/FormCadastro.jsx
@@ -1,53 +1,61 @@
-
-import { useEffect, useState } from 'react'
+import { useRef } from 'react'
 import api from '../../api'
+import 'bootstrap/dist/css/bootstrap.css';
 
 
 // eslint-disable-next-line import/no-anonymous-default-export
 const Formulario = () => {
 
-    const[clientes, setClientes] = useState([])
+    const nome = useRef()
+    const endereco = useRef()
+    const telefone = useRef()
+    const cpf = useRef()
+    const email = useRef()
 
-useEffect(()=>{
-   api.get('/clientes').then(res=>{
-
-    setClientes(res.data);
-    console.log(res)
-
-    }).catch(error=>console.log(error))
-}, [])
-
+    function enviarDados(event){
+        event.preventDefault()
+        api.post("/clientes/",{
+            nome: nome.current.value,
+            foto: endereco.current.value,
+            telefone: telefone.current.value,
+            cpf: cpf.current.value,
+            email: email.current.value
+        }).then((res)=>console.log(res.data)).catch((err)=>console.log(err))
+    }
 
   return (
     <>
        <main class="vh-100 ">
-        <section class="container col-8">
+       <form className="col-8 container" onSubmit={enviarDados}>        
             <h1 class="display-3 mt-4">Cadastrar Cliente</h1>
             <div class="input-group flex-nowrap mt-4">
                 <span class="input-group-text" id="addon-wrapping">Nome</span>
-                <input type="text" class="form-control" placeholder="Nome" aria-label="Username" aria-describedby="addon-wrapping" />
+                <input required type="text" className="form-control" ref={nome} placeholder="Nome" aria-label="Username" aria-describedby="addon-wrapping" />
             </div>
             
             <div class="input-group flex-nowrap mt-4">
                 <span class="input-group-text" id="addon-wrapping">Endereço</span>
-                <input type="text" class="form-control" placeholder="Endereço" aria-label="Endereço" aria-describedby="addon-wrapping" />
+                <input required type="text" className="form-control" ref={endereco} placeholder="Endereço" aria-label="Endereço" aria-describedby="addon-wrapping" />
             </div>
             
             <div class="input-group flex-nowrap mt-4">
                 <span class="input-group-text" id="addon-wrapping">Telefone</span>
-                <input type="text" class="form-control" placeholder="Telefone" aria-label="Telefone" aria-describedby="addon-wrapping" />
+                <input required type="text" className="form-control" ref={telefone} placeholder="Telefone" aria-label="Telefone" aria-describedby="addon-wrapping" />
             </div>
             
             <div class="input-group flex-nowrap mt-4">
                 <span class="input-group-text" id="addon-wrapping">Email</span>
-                <input type="text" class="form-control" placeholder="Email" aria-label="Email" aria-describedby="addon-wrapping" />
+                <input required type="text" className="form-control" ref={email} placeholder="Email" aria-label="Email" aria-describedby="addon-wrapping" />
             </div>
             
             <div class="input-group flex-nowrap mt-4">
                 <span class="input-group-text" id="addon-wrapping">CPF</span>
-                <input type="text" class="form-control" placeholder="CPF" aria-label="CPF" aria-describedby="addon-wrapping" />
+                <input required type="text" className="form-control" ref={cpf} placeholder="CPF" aria-label="CPF" aria-describedby="addon-wrapping" />
             </div>
-        </section>
+            <div className="col s12">
+                            <button className="waves-light btn" type="submit">Enviar</button>
+                        </div>
+            </form>
 
     </main>
     </>
